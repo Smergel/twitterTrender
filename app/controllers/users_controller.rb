76a77e481865f7c1c88a0f16a1995abec4d0ff61
Jacob Smergel <jacob.smergel@gmail.com>
2015-08-04 14:01:24 -0400

    getting ready to pull

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -69,6 +69,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:user_name, :email, :password_hash, :password_salt, :admin)
+      params.require(:user).permit(:user_name, :email, :password, :admin)
     end
 end
