0246f6d643a645ad9b3aa34d4f5132296ff60b7b
William Bolton <william.ellet@gmail.com>
2015-08-04 16:50:53 -0400

    fake data

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,22 +1,19 @@
 class HomeController < ApplicationController
   def index
-  # begin
-    @client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = "tbAVrOKqVOIcL1oF0rKadJ6u7"
-      config.consumer_secret     = "Rv8iygnmryB6Zuffsx8T3R6A45ZlrnpPCZjRf62nAhdff1qqia"
-      config.access_token        = "353428664-2F0iPSHITiDwvJAf7Wuw3AKNZBwEdcztkzHLFeR5"
-      config.access_token_secret = "E9iViX7nALHloE3pckYwbEPtzytu6cgNjfxnGsIcgoLT3"
-  end
+    # @client = Twitter::REST::Client.new do |config|
+    #   config.consumer_key        = "tbAVrOKqVOIcL1oF0rKadJ6u7"
+    #   config.consumer_secret     = "Rv8iygnmryB6Zuffsx8T3R6A45ZlrnpPCZjRf62nAhdff1qqia"
+    #   config.access_token        = "353428664-2F0iPSHITiDwvJAf7Wuw3AKNZBwEdcztkzHLFeR5"
+    #   config.access_token_secret = "E9iViX7nALHloE3pckYwbEPtzytu6cgNjfxnGsIcgoLT3"
+    # end
 
-    @location1 = Location.find(1)
-    @location2 = Location.find(10)
-    @location3 = Location.find(22)
+    @feed = [{:name=>"#ArchThatBackChallenge", :query=>"%23ArchThatBackChallenge", :url=>"http://twitter.com/search?q=%23ArchThatBackChallenge", :promoted_content=>nil}, {:name=>"Kelly Osbourne", :query=>"%22Kelly+Osbourne%22", :url=>"http://twitter.com/search?q=%22Kelly+Osbourne%22", :promoted_content=>nil}, {:name=>"Dave Dombrowski", :query=>"%22Dave+Dombrowski%22", :url=>"http://twitter.com/search?q=%22Dave+Dombrowski%22", :promoted_content=>nil}, {:name=>"Lenny Kravitz", :query=>"%22Lenny+Kravitz%22", :url=>"http://twitter.com/search?q=%22Lenny+Kravitz%22", :promoted_content=>nil}, {:name=>"#ActionMoviesTaughtUs", :query=>"%23ActionMoviesTaughtUs", :url=>"http://twitter.com/search?q=%23ActionMoviesTaughtUs", :promoted_content=>nil}, {:name=>"Perrie", :query=>"Perrie", :url=>"http://twitter.com/search?q=Perrie", :promoted_content=>nil}, {:name=>"#WhenNoOneIsLooking", :query=>"%23WhenNoOneIsLooking", :url=>"http://twitter.com/search?q=%23WhenNoOneIsLooking", :promoted_content=>nil}, {:name=>"Arian Foster", :query=>"%22Arian+Foster%22", :url=>"http://twitter.com/search?q=%22Arian+Foster%22", :promoted_content=>nil}, {:name=>"#WHDemoDay", :query=>"%23WHDemoDay", :url=>"http://twitter.com/search?q=%23WHDemoDay", :promoted_content=>nil}, {:name=>"Miss Piggy", :query=>"%22Miss+Piggy%22", :url=>"http://twitter.com/search?q=%22Miss+Piggy%22", :promoted_content=>nil}]
 
-    @woeid = [@location1, @location2, @location3]
-    
-    @feed = @client.trends(id = x.woeid)
-  # rescue
-  #   flash[:notice] = "It looks like something has gone wrong"
+      @location1 = Location.find(1)
+      @location2 = Location.find(10)
+      @location3 = Location.find(22)
 
+      @woeid = [@location1, @location2, @location3]
+    end
 end
 
