3ec9a6568674ee6bab294f870572d2845fca6ee5
Jacob Smergel <jacob.smergel@gmail.com>
2015-08-04 16:15:56 -0400

    home page changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,7 @@ class HomeController < ApplicationController
       config.consumer_secret     = "Rv8iygnmryB6Zuffsx8T3R6A45ZlrnpPCZjRf62nAhdff1qqia"
       config.access_token        = "353428664-2F0iPSHITiDwvJAf7Wuw3AKNZBwEdcztkzHLFeR5"
       config.access_token_secret = "E9iViX7nALHloE3pckYwbEPtzytu6cgNjfxnGsIcgoLT3"
-  end
+    end
 
     @location1 = Location.find(1)
     @location2 = Location.find(10)
@@ -14,9 +14,8 @@ class HomeController < ApplicationController
 
     @woeid = [@location1, @location2, @location3]
     
-    @feed = @client.trends(id = x.woeid)
   # rescue
   #   flash[:notice] = "It looks like something has gone wrong"
-
+  end
 end
 
