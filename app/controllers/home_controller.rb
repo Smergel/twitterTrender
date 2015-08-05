5290392bf0b4c5018b107007591e38e34ab11977
Jacob Smergel <jacob.smergel@gmail.com>
2015-08-05 09:51:00 -0400

    morining push

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,14 +9,20 @@ class HomeController < ApplicationController
 
     @feed = [{:name=>"#ArchThatBackChallenge", :query=>"%23ArchThatBackChallenge", :url=>"http://twitter.com/search?q=%23ArchThatBackChallenge", :promoted_content=>nil}, {:name=>"Kelly Osbourne", :query=>"%22Kelly+Osbourne%22", :url=>"http://twitter.com/search?q=%22Kelly+Osbourne%22", :promoted_content=>nil}, {:name=>"Dave Dombrowski", :query=>"%22Dave+Dombrowski%22", :url=>"http://twitter.com/search?q=%22Dave+Dombrowski%22", :promoted_content=>nil}, {:name=>"Lenny Kravitz", :query=>"%22Lenny+Kravitz%22", :url=>"http://twitter.com/search?q=%22Lenny+Kravitz%22", :promoted_content=>nil}, {:name=>"#ActionMoviesTaughtUs", :query=>"%23ActionMoviesTaughtUs", :url=>"http://twitter.com/search?q=%23ActionMoviesTaughtUs", :promoted_content=>nil}, {:name=>"Perrie", :query=>"Perrie", :url=>"http://twitter.com/search?q=Perrie", :promoted_content=>nil}, {:name=>"#WhenNoOneIsLooking", :query=>"%23WhenNoOneIsLooking", :url=>"http://twitter.com/search?q=%23WhenNoOneIsLooking", :promoted_content=>nil}, {:name=>"Arian Foster", :query=>"%22Arian+Foster%22", :url=>"http://twitter.com/search?q=%22Arian+Foster%22", :promoted_content=>nil}, {:name=>"#WHDemoDay", :query=>"%23WHDemoDay", :url=>"http://twitter.com/search?q=%23WHDemoDay", :promoted_content=>nil}, {:name=>"Miss Piggy", :query=>"%22Miss+Piggy%22", :url=>"http://twitter.com/search?q=%22Miss+Piggy%22", :promoted_content=>nil}]
 
-      @location1_id = User.find(session[:user_id]).location1 if session[:user_id]
-      @location2_id = User.find(session[:user_id]).location2 if session[:user_id]
-      @location3_id = User.find(session[:user_id]).location3 if session[:user_id]
-
+    if !session[:user_id].nil? 
+      @location1_id = User.find(session[:user_id]).location1
+      @location2_id = User.find(session[:user_id]).location2 
+      @location3_id = User.find(session[:user_id]).location3 
+    
       @location1 = Location.find(@location1_id)
       @location2 = Location.find(@location2_id)
       @location3 = Location.find(@location3_id)
-
+    else
+      @location1 = Location.find(1)
+      @location2 = Location.find(11)
+      @location3 = Location.find(28)
+    end
+    
       @woeid = [@location1, @location2, @location3]
       
   end
